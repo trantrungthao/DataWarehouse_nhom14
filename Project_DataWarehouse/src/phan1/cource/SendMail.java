e85d451b9541e9afc532194ab4d0a6be61c25a9d
trantrungthao <55526199+trantrungthao@users.noreply.github.com>
2020-07-09 15:11:44 +0700

    Cap nhat class  SendMail
    Cap nhat class  SendMail

---
+++ Project_DataWarehouse/src/phan1/cource/SendMail.java
@@ -0,0 +1,47 @@
+package phan1.cource;
+
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendMail {
+	public static boolean sendMail(String to, String subject, String text) {
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("trantrungthao@gmail.com", "12345678");
+            }
+        });
+        try {
+            Message message = new MimeMessage(session);
+            message.setHeader("Content-Type", "text/plain; charset=UTF-8");
+            message.setFrom(new InternetAddress("trantrungthao@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+            message.setSubject(subject);
+            message.setText(text);
+            Transport.send(message);
+        } catch (MessagingException e) {
+            return false;
+        }
+        
+        return true;
+
+	}
+	public static void main(String[] args) {
+		System.out.println(new SendMail().sendMail("16130460@st.hcmuaf.edu.vn", "Data Warehouse", "file error"));
+		
+		
+	}
+}
